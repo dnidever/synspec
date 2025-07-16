110b81eb20432e039cdf29bc24ae11efe36e0d27
David Nidever <dnidever@montana.edu>
2025-07-16 16:23:48 -0600

    added more elements to atomic.py

--- python/synspec/atomic.py
+++ python/synspec/atomic.py
@@ -6,7 +6,9 @@ def periodic(n):
                      'Na','Mg','Al','Si','P','S','Cl','Ar',
                      'K','Ca','Sc','Ti','V','Cr','Mn','Fe','Co','Ni','Cu','Zn','Ga','Ge','As','Se','Br','Kr',
                      'Rb','Sr','Y','Zr','Nb','Mo','Tc','Ru','Rh','Pd','Ag','Cd','In','Sn','Sb','Te','I','Xe',
-                     'Cs','Ba','La','Ce','Pr','Nd'])
+                     'Cs','Ba','La','Ce','Pr','Nd','Pm','Sm','Eu','Gd','Tb','Dy','Ho','Er','Tm','Yb','Lu','Hf',
+                     'Ta','W','Re','Os','Ir','Pt','Au','Hg','Tl','Pb','Bi','Po','At','Rn','Fr','Ra','Ac','Th',
+                     'Pa','U','Np','Pu','Am'])
     if isinstance(n,str):
         j = np.where(elem == n)[0]
         return j+1
